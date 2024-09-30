1739a46219fec3a125b98b77486c5c20c55bb94c
Nicole-Str <nicole.storchak@gmail.com>
2024-09-30 14:21:10 +0300

    Initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+// import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class AppService {
+  // constructor(private configService: ConfigService) {}
+
+  // getHello(): string {
+  //   const name = this.configService.get<string>("POTATO");
+  //   const howManyPotatoes = this.configService.get<number>("POTATO_COUNT");
+  //   console.log("name", name);
+  //   console.log("howManyPotatoes", howManyPotatoes);
+  //   return `Look at my ${howManyPotatoes} ${name}s`;
+  // }
+}
