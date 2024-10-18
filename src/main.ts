d9a74f8442f34e982c0b41821b3a65a0e6cc00e9
Nicole-Str <nicole.storchak@gmail.com>
2024-10-18 19:41:24 +0300

    more

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("before everything", process.env.POTATO)
   await app.listen(3000);
 }
 bootstrap();
